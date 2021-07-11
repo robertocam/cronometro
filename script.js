d8ec0809e9a2fac367210b70b9388c21a3f7fdb7
Roberto Ribeiro <robert.58@hotmail.com>
2021-07-10 21:53:05 -0300

    second commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", () => {
 
-  //Tempo, em segundos, que querenos.
+  //Time, in seconds, we want.
   let sec = 180;
 
   const countDiv = document.getElementById("timer");
@@ -8,27 +8,27 @@ window.addEventListener("load", () => {
   const secpass = () => {
 
     let min = Math.floor(sec/60);
-    let segundosRestantes = sec % 60;
+    let secondsLeft = sec % 60;
 
-    if (segundosRestantes < 10) {
-      segundosRestantes = "0" + segundosRestantes;
+    if (secondsLeft < 10) {
+      secondsLeft = "0" + secondsLeft;
     }
 
     if (min < 10) {
       min = "0" + min;
     }
 
-    //Vai gerar o formato de 02:45
-    countDiv.innerHTML =min + ":" + segundosRestantes;
+    //It will generate the format of 02:45
+    countDiv.innerHTML = min + ":" + secondsLeft;
 
     if (sec > 0) {
       sec = sec -1;
     }
     
     else {
-      countDiv.innerHTML= "Acabou!";
+      countDiv.innerHTML= "He finished!!";
     }
   };
 
   const countDown = setInterval(() => secpass(), 1000);
-})
+});
